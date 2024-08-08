ad2518d49bccf29861aba04114f3bc453a00eebe
Rishav-R03 <rishav042023@gmail.com>
2024-08-08 23:08:11 +0530

    Project section start

--- react-portfolio/src/components/Projects/Projects.jsx
+++ react-portfolio/src/components/Projects/Projects.jsx
@@ -1,1 +1,13 @@
 import React from "react";
+
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className="Project-container">
+        <h1>Here are my project lists</h1>
+    </div>
+  )
+}
+
+export default Projects
